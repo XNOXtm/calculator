cad9046f603607703a5548a0f24ab7104ee75f74
XNOXtm <tusharamishra2006@gmail.com>
2026-01-18 13:09:47 +0530

    Made a function for math operation

--- script.js
+++ script.js
@@ -6,7 +6,4 @@ const multiply = (num1, num2) => {return num1 * num2};
 
 const divide = (num1, num2) => {return num1 / num2};
 
-console.log(add(2,3));
-console.log(subtract(2,3));
-console.log(multiply(2,3));
-console.log(divide(2,3));
+const operate = (operation, num1, num2) => {return operation(num1, num2)};
